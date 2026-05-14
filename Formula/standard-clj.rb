6e357b018cf88c3ff146b8c09265e1cd12970895
Chris Oakman <chris@oakmac.com>
2026-05-14 09:52:33 -0500

    update Standard Clojure Style to v0.28.0

--- Formula/standard-clj.rb
+++ Formula/standard-clj.rb
@@ -17,25 +17,25 @@ class StandardClj < Formula
   desc "Formatter for Clojure code using Standard Clojure Style"
   homepage "https://github.com/oakmac/standard-clojure-style-js"
   license "ISC"
-  version "0.27.0"
+  version "0.28.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.27.0/standard-clj-macos-aarch64"
-      sha256 "1e7b35f12d9de5844a955d3d3a41d6fd6b57f1cd509018d6fd93065398b2b055"
+      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.28.0/standard-clj-macos-aarch64"
+      sha256 "9290f8c5b2899bcd9e10b4e6554dc37859a24f5a21e0c62bc17d5e11acb14db7"
     else
-      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.27.0/standard-clj-macos-x86_64"
-      sha256 "5769162d733cd5f60dbd0acbc2c7b8ddf3dd0881b9f7ad991929c0bf90c52921"
+      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.28.0/standard-clj-macos-x86_64"
+      sha256 "875f3507a14c8ed2fc06db05d8f4c8400e54a30e701c41cce1a2d6c683b86161"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.27.0/standard-clj-linux-aarch64"
-      sha256 "00d66e12eed3cefb89326fba428375395ed30b37842239b90ca0714ea96c9b7a"
+      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.28.0/standard-clj-linux-aarch64"
+      sha256 "9a1d6554c577f95fb70b72a5fbfe87d3ea1d05194664b15408cd13d3c93e7510"
     else
-      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.27.0/standard-clj-linux-x86_64"
-      sha256 "76a756735a0f487256c1862250754e223cb8d8f430bc743e42f55e793c229da3"
+      url "https://github.com/oakmac/standard-clojure-style-js/releases/download/v0.28.0/standard-clj-linux-x86_64"
+      sha256 "4ab58b4e69605ff6f99a90088845b26e781b968ee0ce84478a85df41d239680e"
     end
   end
 
